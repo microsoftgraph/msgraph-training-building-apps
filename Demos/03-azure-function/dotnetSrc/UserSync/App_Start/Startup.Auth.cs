d9d61b35207cd22c56f894bc3aee7451726e8f98
Julie Turner <julie@julieturner.net>
2018-09-17 12:47:12 -0400

    Lab and powerpoint review complete.

--- Demos/03-azure-function/dotnetSrc/UserSync/App_Start/Startup.Auth.cs
+++ Demos/03-azure-function/dotnetSrc/UserSync/App_Start/Startup.Auth.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens;
-using System.Linq;
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
 using System.Threading.Tasks;
-using System.Web;
-using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Notifications;
@@ -15,38 +36,35 @@ namespace UserSync
 {
     public partial class Startup
     {
-        public static string clientId = "29cd97c3-4e7d-4ca3-a592-c25b75dda2e0";
-        public static string clientSecret = "ymphKUQ559vwvIVBZ75?{$;";
+        public static string clientId = "[Enter your client ID, as obtained from the app registration portal]";
+        public static string clientSecret = "[Enter your client secret, as obtained from the app registration portal]";
         private static string authority = "https://login.microsoftonline.com/common/v2.0";
         public static string redirectUri = "https://localhost:44316/";
 
         private void ConfigureAuth(IAppBuilder app)
-        {   
+        {
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
 
-            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions {
-                ClientId = clientId,
-                Authority = authority,
-                RedirectUri = redirectUri,
-                PostLogoutRedirectUri = redirectUri,
-                Scope = "openid profile",
-                ResponseType = "id_token",
-                TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = false,
-                    NameClaimType = "name"
-                },
-                Notifications = new OpenIdConnectAuthenticationNotifications
-                {
-                    AuthenticationFailed = OnAuthenticationFailed,
-                    SecurityTokenValidated = OnSecurityTokenValidated,
-                }
-            });
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+                                               {
+                                                   ClientId = clientId,
+                                                   Authority = authority,
+                                                   RedirectUri = redirectUri,
+                                                   PostLogoutRedirectUri = redirectUri,
+                                                   Scope = "openid profile",
+                                                   ResponseType = "id_token",
+                                                   TokenValidationParameters = new TokenValidationParameters { ValidateIssuer = false, NameClaimType = "name" },
+                                                   Notifications = new OpenIdConnectAuthenticationNotifications
+                                                                   {
+                                                                       AuthenticationFailed = this.OnAuthenticationFailedAsync,
+                                                                       SecurityTokenValidated = this.OnSecurityTokenValidatedAsync
+                                                                   }
+                                               });
         }
 
-        private Task OnSecurityTokenValidated(SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        private Task OnSecurityTokenValidatedAsync(SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
         {
             // Make sure that the user didn't sign in with a personal Microsoft account
             if (notification.AuthenticationTicket.Identity.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value == "9188040d-6c67-4c5b-b112-36a304b66dad")
@@ -58,7 +76,7 @@ namespace UserSync
             return Task.FromResult(0);
         }
 
-        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        private Task OnAuthenticationFailedAsync(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
         {
             notification.HandleResponse();
             notification.Response.Redirect("/Home/Error");
--- Demos/03-azure-function/dotnetSrc/UserSync/Models/MsGraphUser.cs
+++ Demos/03-azure-function/dotnetSrc/UserSync/Models/MsGraphUser.cs
@@ -1,14 +1,40 @@
-﻿using System.Collections.Generic;
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace UserSync.Models
 {
-    public class MsGraphUser
+    public class MSGraphUser
     {
         [JsonProperty(PropertyName = "@odata.type")]
         public string odataType { get; set; }
+
         [JsonProperty(PropertyName = "@odata.id")]
         public string odataId { get; set; }
+
         public List<string> businessPhones { get; set; }
         public string displayName { get; set; }
         public string givenName { get; set; }
@@ -26,6 +52,7 @@ namespace UserSync.Models
     {
         [JsonProperty(PropertyName = "@odata.context")]
         public string context { get; set; }
-        public List<MsGraphUser> value { get; set; }
+
+        public List<MSGraphUser> value { get; set; }
     }
 }
